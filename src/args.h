a29836b91013caccd9e959d42f6ac0c9932f3a8a
TzuHuanTai <tzu.huan.tai@gmail.com>
2025-05-07 17:26:54 -0700

    feat(cli): rename all CLI options to use hyphen-case

--- src/args.h
+++ src/args.h
@@ -7,28 +7,35 @@
 #include <linux/videodev2.h>
 
 struct Args {
+    // video input
     int cameraId = 0;
     int fps = 30;
     int width = 640;
     int height = 480;
-    int jpeg_quality = 30;
     int rotation_angle = 0;
-    int sample_rate = 44100;
-    int file_duration = 60;
-    int peer_timeout = 10;
-    bool no_audio = false;
-    bool hw_accel = false;
-    bool no_adaptive = false;
     bool use_libcamera = false;
     uint32_t format = V4L2_PIX_FMT_MJPEG;
     std::string camera = "libcamera:0";
     std::string v4l2_format = "mjpeg";
+
+    // audio input
+    int sample_rate = 44100;
+    bool no_audio = false;
+
+    // recording
+    std::string record_path = "";
+    int file_duration = 60;
+
+    // webrtc
+    int jpeg_quality = 30;
+    int peer_timeout = 10;
+    bool hw_accel = false;
+    bool no_adaptive = false;
     std::string uid = "";
     std::string stun_url = "stun:stun.l.google.com:19302";
     std::string turn_url = "";
     std::string turn_username = "";
     std::string turn_password = "";
-    std::string record_path = "";
 
     // mqtt signaling
     bool use_mqtt = false;
