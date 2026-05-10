6250f4d08c2f9cefdbebd596a21aa1e47944e584
TzuHuanTai <tzu.huan.tai@gmail.com>
2026-05-10 06:03:37 -0700

    feat: add timestamp and ANSI colors to all log messages

--- src/common/logging.h
+++ src/common/logging.h
@@ -1,19 +1,48 @@
 #ifndef LOGGING_H
 #define LOGGING_H
 
+#include <chrono>
 #include <cstdio>
+#include <filesystem>
+#include <format>
 #include <string>
+#include <unistd.h>
 
-std::string GetFileName(const std::string &file_path);
+inline std::string GetFolderName(const std::string &file_path) {
+    return std::filesystem::path(file_path).parent_path().filename().string();
+}
+
+inline std::string GetFileName(const std::string &file_path) {
+    return std::filesystem::path(file_path).filename().string();
+}
+
+inline std::string GetCurrentTime() {
+    auto now = std::chrono::system_clock::now();
+    auto zoned_time = std::chrono::zoned_time{std::chrono::current_zone(), now};
+    return std::format("{:%T}", zoned_time);
+}
 
 #ifdef DEBUG_MODE
-#define DEBUG_PRINT(fmt, ...) printf("[%s] " fmt "\n", GetFileName(__FILE__).c_str(), ##__VA_ARGS__)
+#define DEBUG_PRINT(fmt, ...)                                                                      \
+    printf("[%s] [%d] \033[1;33mDEBUG\033[0m \033[1;37m%s\033[0m \033[1;34m%s:%d\033[0m " fmt      \
+           "\n",                                                                                   \
+           GetCurrentTime().c_str(), (int)gettid(), GetFolderName(__FILE__).c_str(),               \
+           GetFileName(__FILE__).c_str(), __LINE__, ##__VA_ARGS__)
 #else
 #define DEBUG_PRINT(fmt, ...)
 #endif
 
 #define ERROR_PRINT(fmt, ...)                                                                      \
-    fprintf(stderr, "[%s] Error: " fmt "\n", GetFileName(__FILE__).c_str(), ##__VA_ARGS__)
-#define INFO_PRINT(fmt, ...) printf("[%s] " fmt "\n", GetFileName(__FILE__).c_str(), ##__VA_ARGS__)
+    fprintf(stderr,                                                                                \
+            "[%s] [%d] \033[1;31mERROR\033[0m \033[1;37m%s\033[0m \033[1;34m%s:%d\033[0m " fmt     \
+            "\n",                                                                                  \
+            GetCurrentTime().c_str(), (int)gettid(), GetFolderName(__FILE__).c_str(),              \
+            GetFileName(__FILE__).c_str(), __LINE__, ##__VA_ARGS__)
+
+#define INFO_PRINT(fmt, ...)                                                                       \
+    printf("[%s] [%d] \033[1;32m INFO\033[0m \033[1;37m%s\033[0m \033[1;34m%s:%d\033[0m " fmt      \
+           "\n",                                                                                   \
+           GetCurrentTime().c_str(), (int)gettid(), GetFolderName(__FILE__).c_str(),               \
+           GetFileName(__FILE__).c_str(), __LINE__, ##__VA_ARGS__)
 
 #endif // LOGGING_H
